f4e1386b3acb59596e3eed36dcda97deec007eed
larissaOjeda <larissa.ojeda22@gmail.com>
2023-05-02 13:14:53 -0600

    add initial configuration strcuture

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask 
+
+from config import config
+
+#Routes
+
+app = Flask(__name__)
+
+def page_not_found(error): 
+    text = 'Page not found'
+    return text
+
+if __name__ == '__main__':
+    app.config.from_object(config['development']) # from the dictionary
+    
+    #Blueprints
+
+    # Error handlers
+    app.register_error_handler(404, page_not_found)
+    
+    app.run()
